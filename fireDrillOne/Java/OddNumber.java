a0f1373016bb50da0be1bc719caf93de1e1fbc44
Eddy-star223 <eddyeffanga@gmail.com>
2025-06-17 17:23:41 +0100

    fireDrillOne

---
+++ fireDrillOne/Java/OddNumber.java
@@ -0,0 +1,10 @@
+public class OddNumber {
+    public static void main(String[] args) {
+        
+	for (int counter = 1; counter <= 10; counter++) {
+	if (counter % 2 != 0){
+	System.out.println(counter);
+}
+}
+}
+}
